ca8f5652cb766849dd88d0f65ef1c4436c5179b9
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2016-11-16 18:18:15 +0800

    Primera Version
    Avance del programa

---
+++ HM2_16112016.R
@@ -0,0 +1,23 @@
+#Object Oriented Programing 221841
+#Assignment No.1
+#ID: 105AG8410
+#Name: Bolívar Solarte
+#Date:18/11/2016
+
+#setwd(choose.dir(getwd(), "Select your working folder"))
+
+source("functions.R")
+fileName <- 'test.txt'
+#fileName <- 'article.txt'
+
+article<-readChar(fileName, file.info(fileName)$size);rm(fileName)
+
+#post - processing Data
+
+words<-deleteSpaces(article)
+words<-filterPuntationMarks(words)
+words 
+
+
+
+
---
+++ functions.R
@@ -0,0 +1,25 @@
+deleteSpaces<-function(variable){
+  
+  #return vector with each word in "variable" taking off extras spaces. "varaiable"
+  #can be a vector of charaters
+  
+  variable<-unlist(strsplit(variable," "))
+  variable<-variable[nchar(variable)!=0]
+  return(variable)
+}
+
+filterPuntationMarks<-function(stringVariable){
+  
+  stringVariable<- gsub(pattern = "[[:punct:]]", replacement = "", stringVariable)
+  
+  punct<- " 's  \\r \\t \\nBy  \\n "
+  punct<-deleteSpaces(punct)
+  
+  for (i in 1:length(punct)){
+    stringVariable<-gsub(punct[i]," ",stringVariable)
+  } 
+  stringVariable<-deleteSpaces(stringVariable)
+  return(stringVariable)
+  
+  
+}
